5fce059f6525cf2fc987cf8aa16d4821cd8d0577
latyusDev <yunusabdullateef95@gmail.com>
2025-11-08 08:29:23 +0100

    fixed bug caused by dialog title

--- client/src/components/home/DesignSearchModal.jsx
+++ client/src/components/home/DesignSearchModal.jsx
@@ -25,25 +25,26 @@ const DesignSearchModal = ({isSearchModal,setIsSearchModal}) => {
     
           <Dialog open={isSearchModal} onOpenChange={setIsSearchModal} className={'relative z-50'}>
                   <DialogContent className={'sm:max-w-[1200px] p-5 gap-0 h-[550px] overflow-y-auto'}>
-                    <div className="flex-1 w-full mt-8 md:mt-0 md:w-[700px]  mx-auto relative">
-            <Search className='absolute top-7 left-3  transform -translate-y-1/2 h-5 w-5 text-gray-400'/>
-            <Input
-            onChange={(e)=>setSearchData(e.target.value)}
-            defaultValue={searchData}
-            className='pl-10 py-6 border-gray-200 w-full  bg-gray-50 focus-visible:ring-purple-500'
-            placeholder='Search your projects and canvas'  />
-
-        </div>
+                   <DialogTitle>
+                     <div className="flex-1 w-full mt-8 md:mt-0 md:w-[700px]  mx-auto relative">
+                      <Search className='absolute top-7 left-3  transform -translate-y-1/2 h-5 w-5 text-gray-400'/>
+                      <Input
+                      onChange={(e)=>setSearchData(e.target.value)}
+                      defaultValue={searchData}
+                      className='pl-10 py-6 border-gray-200 w-full  bg-gray-50 focus-visible:ring-purple-500'
+                      placeholder='Search your projects and canvas'  />
+                  </div>
+                   </DialogTitle>
                     {
                      isLoading?<DesignLoaders/>:
                           isError ? <h1 className='text-center  text-xl py-20 shadow-md rounded-lg'>
                                 kindly search for your designs
                           </h1>:
                               <div className='mt-5'>
-                                  <DialogTitle className='text-2xl font-bold mb-4 flex items-center'>
+                                  <div className='text-2xl font-bold mb-4 flex items-center'>
                                     <Sparkles className='h-6 w-6 text-yellow-500 mr-2' />
                                     <p>Search results</p>
-                                </DialogTitle>
+                                </div>
                               {
                                 data?.data.length === 0 ?
                                   <h1 className='text-center mt-3'>Ni design found</h1>:
